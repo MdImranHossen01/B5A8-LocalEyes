54fe785abf49699f1681d1d251ef5c6435b177ba
MdImranHossen01 <imranshuvo101@gmail.com>
2025-12-04 11:44:15 +0600

    data fixing

--- src/app/(dashboardlayout)/dashboard/earnings/page.tsx
+++ src/app/(dashboardlayout)/dashboard/earnings/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 import React, { useState } from 'react';
@@ -43,6 +44,7 @@ interface MonthlyEarning {
 interface ChartData {
   name: string;
   value: number;
+  [key: string]: any;
 }
 
 const EarningsPage = () => {
